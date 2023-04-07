f162ee279bd63bf64a0a0bbb0900c907e24687c3
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2023-04-07 23:01:25 +0200

    Update AircraftClass.dart

--- lib/models/AircraftClass.dart
+++ lib/models/AircraftClass.dart
@@ -1,13 +1,23 @@
 class Aircraft {
-  String callsign = "";
-  String aircrafttype = "";
-  String origin = "";
-  String destination = "";
-  String flightrule = "";
-  String sid = "";
-  String assignedSquawk = "";
-  String currentSquawk = "";
-  String initialClimb = "";
+  // aircraft
+  String callsign = "DLH123";
+  String aircrafttype = "A320";
+  String wtc = "M";
+  String currentSquawk = "2000";
+
+  // flightplan data
+  String origin = "EDDL";
+  String destination = "EDDF";
+  String flightrule = "I";
+  String sid = "COL5T";
+  String assignedSquawk = "1000";
+  String initialClimb = "5000";
+
+  // ground movement:
+  String currentTaxiway = "M";
+
+  // other:
+  String assignedRunway = "23L";
 
   Aircraft({required this.callsign, required this.aircrafttype});
 }
