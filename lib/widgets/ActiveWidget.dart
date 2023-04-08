ceccaaa106b4836edccb75541a18bc2c86afd61e
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2023-04-08 21:57:24 +0200

    added dimensions of elements to ActiveWidget

--- lib/widgets/ActiveWidget.dart
+++ lib/widgets/ActiveWidget.dart
@@ -38,67 +38,113 @@ class ActiveAircraftWidget extends StatefulWidget {
 }
 
 class _ActiveAircraftWidgetState extends State<ActiveAircraftWidget> {
-  final _textStyle = TextStyle(fontSize: 22);
+  final _textStyle = TextStyle(fontSize: 18);
   @override
   Widget build(BuildContext context) {
     final widgetHeight = MediaQuery.of(context).size.height;
     final widgetWidth = MediaQuery.of(context).size.width;
-    return Align(
-      alignment: Alignment.centerLeft,
-      child: Wrap(
+
+    final box1 = 1.1 / 21.1 * widgetWidth;
+    final box2 = 2.1 / 21.1 * widgetWidth;
+    final box3 = 1.5 / 21.1 * widgetWidth;
+    final box4 = 3.8 / 21.1 * widgetWidth;
+    final box5 = 3 / 21.1 * widgetWidth;
+    final box6 = 3 / 21.1 * widgetWidth;
+    final box7 = 4.5 / 21.1 * widgetWidth;
+    final box8 = 2.1 / 21.1 * widgetWidth;
+
+    return Container(
+      child: Row(
         children: [
-          SizedBox(
-            width: widgetWidth * 0.01,
-            child: Text(
-              '?',
-              style: _textStyle,
+          Flexible(
+            child: SizedBox(
+              width: box1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Text(
+                      '?',
+                      style: _textStyle,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-          SizedBox(
-            width: 50,
-            height: 100,
-            child: Text(
-              widget.aircraft.assignedRunway,
-              style: _textStyle,
-            ),
-          ),
-          SizedBox(
-            child: Text(widget.aircraft.currentTaxiway, style: _textStyle),
-          ),
-          SizedBox(
-            child: Text(widget.aircraft.callsign, style: _textStyle),
-          ),
-          SizedBox(
-            child: Column(
-              children: [
-                Text(
-                  widget.aircraft.aircrafttype,
+          Flexible(
+            child: SizedBox(
+              width: box2,
+              child: Center(
+                child: Text(
+                  widget.aircraft.assignedRunway,
                   style: _textStyle,
                 ),
-                Text(
-                  widget.aircraft.wtc,
-                  style: _textStyle,
-                )
-              ],
+              ),
             ),
           ),
-          SizedBox(
-            child: Text('E1029', style: _textStyle),
-          ),
-          SizedBox(
-            child: Column(
-              children: [
-                Text(
-                  widget.aircraft.sid,
-                  style: _textStyle,
-                ),
-                Text(
-                  widget.aircraft.destination,
-                  style: _textStyle,
-                )
-              ],
+          Flexible(
+            child: SizedBox(
+              width: box3,
+              child: Text(widget.aircraft.currentTaxiway, style: _textStyle),
             ),
-          )
+          ),
+          Flexible(
+            child: SizedBox(
+              width: box4,
+              child: Text(widget.aircraft.callsign, style: _textStyle),
+            ),
+          ),
+          Flexible(
+            child: SizedBox(
+              width: box5,
+              child: Column(
+                children: [
+                  Text(
+                    widget.aircraft.aircrafttype,
+                    style: _textStyle,
+                  ),
+                  Text(
+                    widget.aircraft.wtc,
+                    style: _textStyle,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Flexible(
+            child: SizedBox(
+              width: box6,
+              child: Text('E1029', style: _textStyle),
+            ),
+          ),
+          Flexible(
+            child: SizedBox(
+              width: box7,
+              child: Column(
+                children: [
+                  Text(
+                    widget.aircraft.sid,
+                    style: _textStyle,
+                  ),
+                  Text(
+                    widget.aircraft.destination,
+                    style: _textStyle,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Flexible(
+              child: SizedBox(
+            width: box8,
+            child: Column(children: [
+              Text(
+                widget.aircraft.airportposition,
+                style: _textStyle,
+              )
+            ]),
+          ))
         ],
       ),
     );
