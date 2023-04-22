28e92c8dd843cf83852aa093f97ac5520db2b331
LeoKle <leon.kleinschmidt@outlook.de>
2023-04-22 10:21:20 +0200

    added header text style

---
+++ lib/globals/TextStyle/WidgetHeaderStyle.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const TextStyle widgetHeaderStyle = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+    decoration: TextDecoration.none);
