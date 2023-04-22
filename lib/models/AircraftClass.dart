a5c655db09bd20cdbe506ffb72c60e4d39e6ab70
LeoKle <leon.kleinschmidt@outlook.de>
2023-04-22 11:41:46 +0200

    updated aircraft class and data

--- lib/models/AircraftClass.dart
+++ lib/models/AircraftClass.dart
@@ -1,24 +1,38 @@
 class Aircraft {
   // aircraft
-  String callsign = "DLH123";
-  String aircrafttype = "A320";
-  String wtc = "M";
-  String currentSquawk = "2000";
+  String? callsign = "DLH123";
+  String? aircrafttype = "A320";
+  String? wtc = "M";
+  String? currentSquawk = "2000";
 
   // flightplan data
-  String origin = "EDDL";
-  String destination = "EDDF";
-  String flightrule = "I";
-  String sid = "COL5T";
-  String assignedSquawk = "1000";
-  String initialClimb = "5000";
+  String? origin = "EDDL";
+  String? destination = "EDDF";
+  String? flightrule = "I";
+  String? sid = "COL5T";
+  String? assignedSquawk = "1000";
+  String? initialClimb = "5000";
 
   // ground movement:
-  String airportposition = "V02";
-  String currentTaxiway = "M";
+  String? airportposition = "V02";
+  String? currentTaxiway = "M";
 
   // other:
-  String assignedRunway = "23L";
+  String? assignedRunway = "23L";
 
-  Aircraft({required this.callsign, required this.aircrafttype});
+  Aircraft({
+    this.callsign,
+    this.aircrafttype,
+    this.wtc,
+    this.currentSquawk,
+    this.origin,
+    this.destination,
+    this.flightrule,
+    this.sid,
+    this.assignedSquawk,
+    this.initialClimb,
+    this.airportposition,
+    this.currentTaxiway,
+    this.assignedRunway,
+  });
 }
